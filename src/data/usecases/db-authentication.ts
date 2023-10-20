95ffa12155003b69d929438943c9e29be0add854
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-10-20 16:10:02 -0300

    refactor: add type to Authentication usecase

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,7 +1,5 @@
 import { Encrypter, HashComparer, LoadAccountByEmailRepository, UpdateAccessTokenRepository } from "@/data/protocols"
-import { Authentication, AuthenticationParams } from "@/domain/usecases"
-import { AuthenticationModel } from "@/domain/models"
-
+import { Authentication } from "@/domain/usecases"
 
 export class DbAuthentication implements Authentication {
   constructor (
@@ -11,7 +9,7 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepositoryStub: UpdateAccessTokenRepository
   ) {}
 
-  async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationModel> {
+  async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
     if (account) {
       const isValid = await this.hashComparer.compare(authenticationParams.password, account.password)
