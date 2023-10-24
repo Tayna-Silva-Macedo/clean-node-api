2354b801abc1e133c2f4b93926926a31a112c5f2
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-10-24 17:46:11 -0300

    refactor: update graphql and apollo server

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,14 +1,17 @@
-import app from '@/main/config/app'
+import { setupApp } from '@/main/config/app'
 import { MongoHelper } from '@/infra/db'
 
 import { hash } from 'bcrypt'
 import { Collection } from 'mongodb'
+import { Express } from 'express'
 import request from 'supertest'
 
 let accountCollection: Collection
+let app: Express
 
 describe('Login Routes', () => {
   beforeAll(async () => {
+    app = await setupApp()
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
